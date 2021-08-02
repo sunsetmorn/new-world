ee101a96ce1c3c00fcec7b0108ea2677f7e36b0e
sunsetmorn <zhaoxq@si-tech.com.cn>
2021-08-02 11:43:48 +0800

    1111

---
+++ src/letcode/algorithm/Solution19.java
@@ -0,0 +1,16 @@
+package letcode.algorithm;
+
+import letcode.commen.ListNode;
+
+/**
+ * 删除链表的倒数第 N 个结点
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ */
+public class Solution19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next == null) return head;
+        
+
+        return head;
+    }
+}
