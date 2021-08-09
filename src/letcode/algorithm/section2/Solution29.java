5e3c85ec34add6464ee66d2b5c034c9967c59e84
sunsetmorn <zhxq6120@163.com>
2021-08-09 22:42:34 +0800

    ttt

---
+++ src/letcode/algorithm/section2/Solution29.java
@@ -0,0 +1,18 @@
+package letcode.algorithm.section2;
+
+/**
+ * 两数相除
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ */
+public class Solution29 {
+    public int divide(int dividend, int divisor) {
+        int a = Math.abs(dividend);
+        int b = Math.abs(divisor);
+        if(dividend >0){
+
+        }
+        return 0;
+    }
+}
