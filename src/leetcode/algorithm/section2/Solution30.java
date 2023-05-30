ecebc2c650934488a492ba2df7c1f460c4b6d364
zhaoxq <zhxq6120@163.com>
2023-05-30 14:33:16 +0800

    20230530

--- src/leetcode/algorithm/section2/Solution30.java
+++ src/leetcode/algorithm/section2/Solution30.java
@@ -1,6 +1,9 @@
 package leetcode.algorithm.section2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 串联所有单词的子串
@@ -8,12 +11,42 @@ import java.util.List;
  * 注意子串要与words中的单词完全匹配，中间不能有其他字符，但不需要考虑words中单词串联的顺序。
  */
 public class Solution30 {
-        public List<Integer> findSubstring(String s, String[] words) {
-        if (s == null || s.length() == 0 || words == null || words.length == 0) return null;
-        int n = words.length;
-        int l = words.length;
-
-
-        return null;
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<Integer>();
+        int m = words.length, n = words[0].length(), ls = s.length();
+        for (int i = 0; i < n; i++) {
+            if (i + m * n > ls) {
+                break;
+            }
+            Map<String, Integer> differ = new HashMap<String, Integer>();
+            for (int j = 0; j < m; j++) {
+                String word = s.substring(i + j * n, i + (j + 1) * n);
+                differ.put(word, differ.getOrDefault(word, 0) + 1);
+            }
+            for (String word : words) {
+                differ.put(word, differ.getOrDefault(word, 0) - 1);
+                if (differ.get(word) == 0) {
+                    differ.remove(word);
+                }
+            }
+            for (int start = i; start < ls - m * n + 1; start += n) {
+                if (start != i) {
+                    String word = s.substring(start + (m - 1) * n, start + m * n);
+                    differ.put(word, differ.getOrDefault(word, 0) + 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                    word = s.substring(start - n, start);
+                    differ.put(word, differ.getOrDefault(word, 0) - 1);
+                    if (differ.get(word) == 0) {
+                        differ.remove(word);
+                    }
+                }
+                if (differ.isEmpty()) {
+                    res.add(start);
+                }
+            }
+        }
+        return res;
     }
 }
--- src/leetcode/algorithm/section2/Solution44.java
+++ src/leetcode/algorithm/section2/Solution44.java
@@ -15,29 +15,55 @@ import java.util.HashMap;
  */
 public class Solution44 {
     public static boolean isMatch(String s, String p) {
-        System.out.println(s);
-        System.out.println(p);
-        if(s.isEmpty() && p.isEmpty()) return true;
-        if(s.equals(p)) return true;
-        if(p.isEmpty() && !s.isEmpty()) return false;
-        if(!p.isEmpty() && s.isEmpty()){
-            return p.replace("*","").length() ==0;
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            if(p.charAt(i - 1) == '*'){
+                dp[0][i] = true;
+            }else{
+                break;
+            }
         }
-        if(s.charAt(0) == p.charAt(0) || p.charAt(0) == '?'){
-            return isMatch(s.substring(1),p.substring(1));
-        }else if(p.charAt(0) == '*'){
-            if(p.length() ==1) return true;
-            for(int i =0;i < s.length();i++){
-                if(isMatch(s.substring(i),p.substring(1))){
-                    return true;
+        for (int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j){
+                if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i-1][j] | dp[i][j-1];
+                }else if(p.charAt(j-1) == '?' || p.charAt(j-1) == s.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1];
                 }
             }
         }
-        return false;
+        return dp[m][n];
     }
 
+    public boolean isMatch2(String s, String p) {
+        int i=0,j=0,iR=-1,jR=-1,i_len=s.length(),j_len=p.length();
+        while (i<i_len){
+            if (j<j_len&&p.charAt(j)=='*'){
+                j++;
+                jR=j;
+                iR=i;
+            }else if (j<j_len&&(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')){
+                i++;
+                j++;
+            }else if (iR>=0){
+                iR++;
+                i=iR;
+                j=jR;
+            }else {
+                return false;
+            }
+        }
+        while (j<j_len&&p.charAt(j)=='*'){
+            j++;
+        }
+        return j==j_len;
+    }
 
     public static void main(String[] args) {
-        isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab","***bba**a*bbba**aab**b");
+        System.out.println(isMatch("aa","*"));
+        System.out.println(isMatch("babaaababaabababbbbbbaabaabbabababbaababbaaabbbaaab", "***bba**a*bbba**aab**b"));
     }
 }
